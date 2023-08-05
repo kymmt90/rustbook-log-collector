7c9dca61139b5fcf138fa310b057f94ee81b6afc
Kohei Yamamoto <kymmt90@gmail.com>
2023-08-05 18:25:14 +0900

    Server stub

---
+++ server/src/handlers.rs
@@ -0,0 +1,7 @@
+use actix_web::Responder;
+
+pub async fn handle_get_logs() -> impl Responder {
+    todo!();
+
+    ""
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::web;
+use actix_web::App;
+use actix_web::HttpServer;
+
+mod handlers;
+
+#[derive(Clone)]
+pub struct Server {}
+
+impl Server {
+    pub fn new() -> Self {
+        Server {}
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use crate::handlers::*;
+
+    HttpServer::new(|| {
+        App::new()
+            .app_data(web::Data::new(Server {}))
+            .service(web::scope("/logs").route("/", web::get().to(handle_get_logs)))
+    })
+    .bind(("localhost", 3000))?
+    .run()
+    .await
 }
